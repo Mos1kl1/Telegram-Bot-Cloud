03f1bdbba411d6a4511ae0bba74873ead433d481
Мосикли <147040128+Mos1kl1@users.noreply.github.com>
2026-01-22 15:46:11 +0400

    Add logging actions
    Added logging of user actions to identify problems and determine the causes of failures.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import telebot
 import sqlite3
+import logging
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+logger = logging.getLogger(__name__)
 DB_PATH = 'Users.db'
 
+logging.basicConfig(
+    filename='bot.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 
 def init_db():
     with sqlite3.connect(DB_PATH, check_same_thread=False) as conn:
@@ -51,6 +58,7 @@ def start(message):
             btn3 = telebot.types.KeyboardButton("/history")
             markup.add(btn1, btn2, btn3)
             bot.send_message(message.chat.id, "You have successfully registered.", reply_markup=markup)
+            logger.info(f"Registered {user_name}, ID: {user_id}")
 
 
 user_states = {}
@@ -59,8 +67,10 @@ user_states = {}
 @bot.message_handler(func=lambda message: message.text == "/addinfo")
 def add(message):
     user_id = message.from_user.id
+    user_name = message.from_user.first_name
     user_states[user_id] = 'waiting_first_name'
     bot.send_message(message.chat.id, "Write your first name: ")
+    logger.info(f"Name: {user_name}, ID: {user_id}, {message.text}")
 
 
 @bot.message_handler(func=lambda message:
@@ -88,6 +98,7 @@ def get_last_name(message):
             "UPDATE Users SET FirstName = ?, LastName = ? WHERE User_id = ?",
             (first_name, last_name, user_id)
         )
+        logger.info(f"Updated {user_id}, ID: {user_id}")
         conn.commit()
 
     del user_states[user_id]
@@ -97,16 +108,22 @@ def get_last_name(message):
 
 @bot.message_handler(func=lambda message: message.text == "/command")
 def command(message):
+    user_id = message.from_user.id
+    user_name = message.text.strip()
     bot.send_message(message.chat.id, '''
     1) /start - Launch the bot and register in it\n
 2) /addinfo - Add information (First name, Last name)\n
 3) /command - Display all commands\n
 4) /history - Display all user queries\n
     ''')
+    logger.info(f"Name: {user_name}, ID: {user_id}, {message.text}")
 
 @bot.message_handler(func=lambda message: message.text == "/history")
 def history(message):
+    user_id = message.from_user.id
+    user_name = message.text.strip()
     bot.send_message(message.chat.id, '''Your history:''')
+    logger.info(f"Name: {user_name}, ID: {user_id}, {message.text}")
 
 init_db()
 bot.polling(none_stop=True)
